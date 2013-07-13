b81620075f41d5cbcc97d92b2576818afca0e707
Ubuntu <ubuntu@ip-10-194-145-184.ec2.internal>
2013-07-13 07:00:11 +0000

    Adding entry page enhancements for Sujetha.

--- input.php
+++ input.php
@@ -1,12 +1,27 @@
+
 <html>
 <body>
 <title>Meet up Finder</title>
-<h1>Welcome to Meet up finder</h1>
-<h2>Please enter your name and desired meetup location:</h2>
+<div  style="background-color:#FF6666;">
+<div id="header" style="background-color:#990033;">
+<h1 style="margin-bottom:0;clear:both;text-align:center;">Welcome to Meet up finder</h1></div>
+<img src="people.jpg" width="730" height="500"></body>
+
+
+<div style="background-color:#FF6666;height:400px;width:559px;float:right;">
+
 <form action="checkuser.php" method="post">
-Name: <input type="text" name="name">
-Location: <input type="text" name="location">
-<input type="submit">
+
+<h3> Login</h3><br>
+<b> Existing User Please Enter First Name: <input type="text" name="name"></b> <br> <br>
+<input type="submit" value="verify"> <br>
 </form>
+
+<form action="openid/meetupfinder.php" method="post">
+<input type="submit" value="New User"> <br>
+</form>
+
+</div>
+</div>
 </body>
 </html> 
