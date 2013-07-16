ca48686e3ea88bf36363ff3e5e4b42acd0297d7e
Ubuntu <ubuntu@ip-10-194-145-184.ec2.internal>
2013-07-16 07:06:51 +0000

    add javascript validation on location box

--- app/getLocation.php
+++ app/getLocation.php
@@ -1,8 +1,18 @@
-<form action="gotLocation.php" method="post">
+<form action="gotLocation.php" method="post" name="loc" onsubmit="return validateForm()">
 
 <br /><h3> Enter a location where you would like to meetup with your friends:</h3>
 <input type="text" name="location">
 <input type="submit" value="Let's Go!"> 
 </form>
 
-
+<script type="text/javascript">
+function validateForm()
+{
+var x=document.forms["loc"]["location"].value;
+if (x==null || x=="")
+  {
+  alert("Location must be filled in");
+  return false;
+  }
+}
+</script>
